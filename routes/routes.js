bbf69aa15898a0bd02c600ddd37cfb65125f16dd
isaacfito07 <isaacfito07@gmail.com>
2022-05-11 11:49:47 -0600

    Acceso a la ruta de usuarios

--- routes/routes.js
+++ routes/routes.js
@@ -9,5 +9,13 @@ const router = app => {
             message:'Bienvenido a Node.JS Express REST API!';
         });
    });
+   //Mostrar todos los usuarios
+   app.get('/users', (request, response) =>{
+        pool.query('SELECT * FROM users', (error, result) =>{
+            if (error) throw error;
+            
+            response.send(result);
+        });
+   });
 
 };
